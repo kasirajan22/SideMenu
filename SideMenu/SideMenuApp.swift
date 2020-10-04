67ef3634e06103f73c375388d2aa603003a5a8a6
kasirajan22 <kasi.srm@gmail.com>
2020-10-04 10:38:20 +0530

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by Magizhan on 04/10/20.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
